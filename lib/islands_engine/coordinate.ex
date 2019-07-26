32f5e0e906c95039287cbc5fb5ea16473e42b67c
Eduardo Verdeja <eaverdeja@gmail.com>
2019-07-25 21:04:21 -0300

    Modelling coordinates

---
+++ lib/islands_engine/coordinate.ex
@@ -0,0 +1,15 @@
+defmodule IslandsEngine.Coordinate do
+  alias __MODULE__
+
+  @board_range 1..10
+
+  @enforce_keys [:row, :col]
+  defstruct [:row, :col]
+
+  @spec new(any, any) :: {:error, :invalid_coordinate} | {:ok, IslandsEngine.Coordinate.t()}
+  def new(row, col) when row in @board_range and col in @board_range do
+    {:ok, %Coordinate{row: row, col: col}}
+  end
+
+  def new(_row, _col), do: {:error, :invalid_coordinate}
+end
