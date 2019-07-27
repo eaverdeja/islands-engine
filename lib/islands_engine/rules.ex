352a0f00ce6cced65ebf80ad38e50f459431ac3d
Eduardo Verdeja <eaverdeja@gmail.com>
2019-07-27 09:44:17 -0300

    Game states - Initialized

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -5,5 +5,8 @@ defmodule IslandsEngine.Rules do
 
   def new(), do: %Rules{}
 
+  def check(%Rules{state: :initialized} = rules, :add_player),
+    do: {:ok, %Rules{rules | state: :players_set}}
+
   def check(_state, _action), do: :error
 end
