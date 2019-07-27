28a3638d78bb065b290e2a7722f6e0446fe26fcf
Eduardo Verdeja <eaverdeja@gmail.com>
2019-07-27 10:30:56 -0300

    Game states - Players set

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -1,12 +1,33 @@
 defmodule IslandsEngine.Rules do
   alias __MODULE__
 
-  defstruct state: :initialized
+  defstruct state: :initialized,
+            player1: :islands_not_set,
+            player2: :islands_not_set
 
   def new(), do: %Rules{}
 
   def check(%Rules{state: :initialized} = rules, :add_player),
     do: {:ok, %Rules{rules | state: :players_set}}
 
+  def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
+    case Map.fetch!(rules, player) do
+      :islands_set -> :error
+      :islands_not_set -> {:ok, rules}
+    end
+  end
+
+  def check(%Rules{state: :players_set} = rules, {:set_islands, player}) do
+    rules = Map.put(rules, player, :islands_set)
+
+    case both_players_islands_set?(rules) do
+      true -> {:ok, %Rules{rules | state: :player1_turn}}
+      false -> {:ok, rules}
+    end
+  end
+
   def check(_state, _action), do: :error
+
+  defp both_players_islands_set?(rules),
+    do: rules.player1 == :islands_set && rules.player2 == :islands_set
 end
