bca3d6021c0d6db7d241a87cb69b225df1f1d1c9
Eduardo Verdeja <eaverdeja@gmail.com>
2019-07-28 09:24:01 -0300

    Initializing GenServer State

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,3 +1,15 @@
 defmodule IslandsEngine.Game do
   use GenServer
+
+  alias IslandsEngine.{Board, Guesses, Rules}
+
+  def start_link(name) when is_binary(name),
+    do: GenServer.start_link(__MODULE__, name)
+
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+
+    {:ok, %{player1: player1, player2: player2, rules: Rules.new()}}
+  end
 end
